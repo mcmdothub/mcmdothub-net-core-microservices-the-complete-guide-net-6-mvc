a8fc95ccc9ce1bca552e20467fb5e351e110c581
cristianlinear <cristian.marin@linearsystem.it>
2022-12-08 15:09:59 +0100

    Create project Deli.Web

---
+++ Deli.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Deli.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
